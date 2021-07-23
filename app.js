00a45e4fc6201b46cef290af187b509985f6281c
steven4program <stevenchang421@gmail.com>
2021-07-23 21:48:06 +0800

    feat: add handlebars body-parser method-override

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 // require packages used in the project
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const app = express()
 const port = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 mongoose.connect('mongodb://localhost/expense-tracker', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(methodOverride('_method'))
 
 const db = mongoose.connection
 
@@ -22,7 +32,7 @@ db.once('open', () => {
 
 // routes setting
 app.get('/', (req, res) => {
-  res.send('This is my expense tracker built with Express')
+  res.render('index')
 })
 
 // start and listen on the Express server
