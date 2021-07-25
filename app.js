b6940ea2392a7e77d61c3d16af5c58da7e941668
steven4program <stevenchang421@gmail.com>
2021-07-26 00:36:22 +0800

    feat: add category filter function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const helpers = require('./tools/handlebarsHelpers')
+const hbsHelpers = require('handlebars-helpers')(['comparison'])
 
 const routes = require('./routes')
 require('./config/mongoose')
@@ -11,7 +12,10 @@ require('./config/mongoose')
 const app = express()
 const port = 3000
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers }))
+app.engine(
+  'hbs',
+  exphbs({ defaultLayout: 'main', extname: '.hbs', helpers, hbsHelpers })
+)
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,30 +6,30 @@ const router = express.Router()
 const Record = require('../../models/Record')
 const Category = require('../../models/Category')
 
-const categories = []
-Category.find()
-  .lean()
-  .then((category) => categories.push(...category))
-  .catch((error) => console.error(error))
-
 // define home route
 router.get('/', (req, res) => {
   const category = req.query.category
-  const filter = {}
+  const filter = req.query.filter ? { category: req.query.filter } : {}
   if (category) {
     filter.category = category
   }
 
+  // asynchronous
+  const categories = []
+  Category.find()
+    .lean()
+    .then((category) => categories.push(...category))
+    .catch((error) => console.log(error))
+
   Record.find(filter)
     .populate('category')
     .lean()
-    .sort({ date: 'desc' }) // desc in date
     .then((records) => {
       let totalAmount = 0
       records.forEach((record) => (totalAmount += record.amount))
       res.render('index', { categories, category, records, totalAmount })
     })
-    .catch((error) => console.error(error))
+    .catch((error) => console.log(error))
 })
 
 // exports router
