e1d8d48fb3a37e0ee20214525c10984bb4e3e73c
steven4program <stevenchang421@gmail.com>
2021-10-28 22:28:34 +0800

    feat: add facebook login & dotenv

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,15 @@ const home = require('./modules/home')
 const records = require('./modules/records')
 const filter = require('./modules/filter')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
+
 const { authenticator } = require('../middleware/auth')
 
 // direct request corresponding module
 router.use('/records', authenticator, records)
 router.use('/filter', filter)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
