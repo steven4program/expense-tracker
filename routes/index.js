20b75dd6091220b0eec2599dea706092c685292d
steven4program <stevenchang421@gmail.com>
2021-10-28 21:36:52 +0800

    feat: add isAuthenticated to display different view templates

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,12 @@ const home = require('./modules/home')
 const records = require('./modules/records')
 const filter = require('./modules/filter')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
 // direct request corresponding module
-router.use('/', home)
-router.use('/records', records)
+router.use('/records', authenticator, records)
 router.use('/filter', filter)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 module.exports = router
